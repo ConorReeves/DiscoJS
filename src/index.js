005719464430bcb9fc132450ad4ab6297a0d5dd7
ConorReeves <conor@reeves.es>
2025-01-04 05:01:52 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+"use strict";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
